8012a3ecc491eb8fa6d17b79b6905afdf61ba69f
Aurélien <a.trotier@gmail.com>
2020-01-16 12:55:34 +0100

    approximately same value (need test for other direction)

--- xform_mat_aurel.m
+++
@@ -1,9 +0,0 @@
-
-
-
-%% Subfunction: get field if exist, return default value otherwise
-function val = tryGetField(s, field, dftVal)
-if isfield(s, field), val = s.(field); 
-elseif nargin>2, val = dftVal;
-else, val = [];
-end
